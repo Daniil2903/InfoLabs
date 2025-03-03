48f19c930ff99fa341dfa22f874cf161e7f67b5f
Daniil2903 <m2406326@edu.misis.ru>
2025-03-03 14:12:19 +0300

    Update Lab1.py

--- Lab1OfTheSecondSemester/Lab1.py
+++ Lab1OfTheSecondSemester/Lab1.py
@@ -1,20 +1,37 @@
-x=[[1, 1],[1, 2], [2, 1], [2, 2]]
-y=[1, 1, 0, 0]
-p=float(input('Доля:'))
-def damn_x(x, p):
-    x_train=[]
-    for i in range(round(p*len(x))):
-        x_train.append(x[i])
-    return(x_train)
-def damn_y(y, p):
-    y_train=[]
-    for i in range(round(p*len(y))):
-        y_train.append(y[i])
-    return(y_train)
-    
+import random
+xMin1=0
+xMax1=50
+yMin1=0
+yMax1=50
+xMin2=25
+xMax2=75
+yMin2=25
+yMax2=75
 k=3
-import matplotlib.pyplot as plt
-import numpy as np
-fig, ax=plt.subplots()
-ax.scatter(damn_x(x, p), damn_y(y, p), linewidth=2.0)
-plt.show
+classCount=2
+pointCount1=50
+pointCount2=50
+x=[]
+y=[]
+for i in range(pointCount1):
+    x.append([random.uniform(xMin1, xMax1), random.uniform(yMin1, yMax1)])
+for i in range(pointCount2):
+    x.append([random.uniform(xMin2, xMax2), random.uniform(yMin2, yMax2)])
+for i in range(pointCount1):
+    y.append(1)
+for i in range(pointCount2):
+    y.append(0)
+p=0.8
+def TrainAndTest(x, y, p):
+    x_train=[]
+    y_train=[]
+    x_test=[]
+    y_test=[]
+    for i in range(round((pointCount1+pointCount2)*p)):
+        x_train.append(x[i])
+        y_train.append(y[i])
+    for i in range(round((pointCount1+pointCount2)*(1-p))):
+        x_test.append(x[i])
+        y_test.append(y[i])
+    return(x_train, y_train, x_test,  y_test)
+print(TrainAndTest(x, y, p))
