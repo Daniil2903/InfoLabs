3a676946b203f01a0f53fcc1a2cd27085eb41bd2
Daniil2903 <m2406326@edu.misis.ru>
2025-03-15 12:49:02 +0300

    Update HalflabNum1#5.py

--- Halflab2/HalflabNum1#5.py
+++ Halflab2/HalflabNum1#5.py
@@ -8,8 +8,7 @@ x=np.linspace(0, 10, 100)
 y=a*x**2+b
 fig, ax = plt.subplots()
 ax.plot(x, y, linewidth=2.0)
-
-ax.set(xlim=(0, 5), xticks=np.arange(1, 25),
-       ylim=(0, 25), yticks=np.arange(1, 25))
+ax.set(xlim=(0, 5), xticks=np.arange(0, 50),
+       ylim=(0, 25), yticks=np.arange(0, 50))
 
 plt.show()
