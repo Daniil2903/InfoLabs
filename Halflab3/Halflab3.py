b04f883a97990a93d133d5d63f392fd3f1d7277a
Daniil2903 <m2406326@edu.misis.ru>
2025-03-24 01:20:43 +0300

    Add files via upload

---
+++ Halflab3/Halflab3.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+df=pd.read_csv(r'Квартиры.csv',sep=';')
+print(df, df.dtypes)
+df['price']=df['price'].str.replace(',','.') 
+df['price']=df['price'].astype(float)
+plt.scatter(df.area,df.price,color='red')
+plt.xlabel('Площадь(кв.м.)')
+plt.ylabel('Стоимость(млн.руб)')
+plt.show() 
+reg=linear_model.LinearRegression()
+reg.fit(df[['area']],df.price)
+print(reg.predict([[38]]))
+
+print(reg.predict(df[['area']]))
+print(reg.coef_)
+print(reg.intercept_)
+price=reg.coef_ * df.area+reg.intercept_
+plt.scatter(df.area,df.price,color='red')
+plt.xlabel('Площадь(кв.м.)')
+plt.ylabel('Стоимость(млн.руб)')
+plt.plot(df.area, reg.predict(df[['area']]))
+plt.show()
+pred=pd.read_csv(r'Prediction.csv',sep=';')
+print(pred)
+p=reg.predict([pred['area']].values)
+print(p)
+pred['price'] = p
+print(pred)
+pred.to_excel('new.xlsx', index=False)
