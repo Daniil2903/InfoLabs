26d7a0292d946d1e522772d0e13395fffb6741a6
Daniil2903 <m2406326@edu.misis.ru>
2025-03-31 13:34:34 +0300

    Update HalflabNum1#5.py

--- Halflab2/HalflabNum1#5.py
+++ Halflab2/HalflabNum1#5.py
@@ -1,6 +1,17 @@
 #5
 a=int(input('a='))
 b=int(input('b='))
+n=0
+k=0
+if a>10 and b>20:
+       n=a+20
+       k=b+20
+elif a>0 and a<10 and b<20:
+       n=10
+       k=25
+elif a>0 and a<10 and b>20:
+       n=10
+       k=b+20
 import matplotlib.pyplot as plt
 import numpy as np
 plt.style.use('_mpl-gallery')
@@ -8,7 +19,7 @@ x=np.linspace(0, 10, 100)
 y=a*x**2+b
 fig, ax = plt.subplots()
 ax.plot(x, y, linewidth=2.0)
-ax.set(xlim=(0, 5), xticks=np.arange(0, 50),
-       ylim=(0, 25), yticks=np.arange(0, 50))
+ax.set(xlim=(0, 5), xticks=np.arange(0, n),
+       ylim=(0, 25), yticks=np.arange(0, k))
 
 plt.show()
