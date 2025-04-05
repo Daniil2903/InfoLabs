cd03d36a1d4da70b9bf0196ad8ebb0dbaaeff4e6
Daniil2903 <m2406326@edu.misis.ru>
2025-04-05 15:05:36 +0300

    Update Lab1.py

--- Lab1OfTheSecondSemester/Lab1.py
+++ Lab1OfTheSecondSemester/Lab1.py
@@ -1,100 +1,57 @@
 import random
-import heapq
-p = 0.8
-xMin1 = 0
-xMax1 = 50
-yMin1 = 0
-yMax1 = 50
-xMin2 = 25
-xMax2 = 75
-yMin2 = 25
-yMax2 = 75
-k = 3
-classesCount = 2
-pointsCount1 = 50
-pointsCount2 = 50
-x = []
-y = []
-for i in range(pointsCount1):
-    x.append([random.uniform(xMin1,xMax1),random.uniform(yMin1,yMax1)])
+import numpy as np
+import matplotlib.pyplot as plt
+p=0.8
+xMin1, xMax1=0, 50
+yMin1, yMax1=0, 50
+xMin2, xMax2=25, 75
+yMin2, yMax2=25, 75
+pointsCount1=50
+pointsCount2=50
+x=[]
+y=[]
+for _ in range(pointsCount1):
+    x.append([random.uniform(xMin1, xMax1), random.uniform(yMin1, yMax1)])
     y.append(0)
-for j in range(pointsCount2):
-    x.append([random.uniform(xMin2,xMax2),random.uniform(yMin2,yMax2)])
+for _ in range(pointsCount2):
+    x.append([random.uniform(xMin2, xMax2), random.uniform(yMin2, yMax2)])
     y.append(1)
-print(x)     
-print(y)  
-def train_test_split(x,y,p):
+def train_test_split(x, y, p):
     combined=list(zip(x, y))
     random.shuffle(combined)
-    split_idx=int(len(combined)*p)
+    split_idx=int(len(combined) * p)
     train=combined[:split_idx]
     test=combined[split_idx:]
     x_train, y_train=zip(*train)
     x_test, y_test=zip(*test)
-    
-    return x_train, x_test, y_train, y_test
-x_train, x_test, y_train, y_test = train_test_split(x,y,p)                                                                                                        
-def Pred(x_train, y_train, x_test):
-    k=3
-    r=[]
-    indi=[]
-    o12=0
-    x12=0
+    return list(x_train), list(x_test), list(y_train), list(y_test)
+x_train, x_test, y_train, y_test=train_test_split(x, y, p)
+def predict_knn(x_train, y_train, x_test, k):
     y_predict=[]
-    for i in range(len(x_test)):
-        r=[]
-        for j in range(len(x_train)):
-            r.append((((x_train[j][0]-x_test[i][0])**2)+(x_train[j][1]-x_test[i][1])**2)**0.5)
-        a=heapq.nsmallest(k, r)
-        indi=[]
-        for i in range(len(a)):
-            indi.append(r.index(a[i]))
-            if y_train[indi[i]]==1:
-                o12+=1
-            else:
-                x12+=1
-        if o12>x12:
-            y_predict.append(1)
-        else:
-            y_predict.append(2)
-        print(indi)
+    for test_point in x_test:
+        distances=[np.linalg.norm(np.array(train_point)-np.array(test_point)) for train_point in x_train]
+        nearest_indices=np.argsort(distances)[:k]
+        nearest_labels=[y_train[i] for i in nearest_indices]
+        predicted_label=max(set(nearest_labels), key=nearest_labels.count)
+        y_predict.append(predicted_label)
     return y_predict
-y_predict1=Pred(x_train, y_train, x_test)
-sovp=0
-for i in range(len(y_predict1)):
-    if y_test[i]==y_predict1[i]:
-        sovp+=1
-accuracy=sovp/len(y_predict1)
-print('Accuracy:', accuracy)
-import matplotlib.pyplot as plt
-plt.figure(figsize=(10, 6))
-train_0=[p for p, lbl in zip(x_train, y_train) if lbl==0]
-def plot_results(x_train, y_train, x_test, y_test, y_predict1):
+k=5
+y_predict=predict_knn(x_train, y_train, x_test, k)
+sovp=sum(1 for true, pred in zip(y_test, y_predict) if true==pred)
+accuracy = sovp / len(y_predict)
+print(f'Accuracy: {accuracy:.2f}')
+def plot_results(x_train, y_train, x_test, y_test, y_predict):
     plt.figure(figsize=(10, 7))
-    train_0 = [p for p, lbl in zip(x_train, y_train) if lbl == 0]
-    train_1 = [p for p, lbl in zip(x_train, y_train) if lbl == 1]
+    train_0=[p for p, lbl in zip(x_train, y_train) if lbl==0]
+    train_1=[p for p, lbl in zip(x_train, y_train) if lbl==1]
     if train_0:
         plt.scatter(*zip(*train_0), c='blue', marker='o', label='Тренировочный ноль')
     if train_1:
         plt.scatter(*zip(*train_1), c='blue', marker='x', label='Тренировочная единица')
-    correct_0 = []
-    correct_1 = []
-    wrong_0 = []
-    wrong_1 = []
-    for i in range(len(x_test)):
-        point = x_test[i]
-        true_lbl = y_test[i]
-        pred_lbl = y_predict1[i]
-        if true_lbl == pred_lbl:
-            if true_lbl == 0:
-                correct_0.append(point)
-            else:
-                correct_1.append(point)
-        else:
-            if true_lbl == 0:
-                wrong_0.append(point)
-            else:
-                wrong_1.append(point)
+    correct_0=[p for p, t, pred in zip(x_test, y_test, y_predict) if t==pred and t==0]
+    correct_1=[p for p, t, pred in zip(x_test, y_test, y_predict) if t==pred and t==1]
+    wrong_0=[p for p, t, pred in zip(x_test, y_test, y_predict) if t!=pred and t==0]
+    wrong_1=[p for p, t, pred in zip(x_test, y_test, y_predict) if t!=pred and t==1]
     if correct_0:
         plt.scatter(*zip(*correct_0), c='green', marker='o', alpha=0.6, label='Правильный ноль')
     if correct_1:
@@ -103,12 +60,7 @@ def plot_results(x_train, y_train, x_test, y_test, y_predict1):
         plt.scatter(*zip(*wrong_0), c='red', marker='o', alpha=0.6, label='Неправильный ноль')
     if wrong_1:
         plt.scatter(*zip(*wrong_1), c='red', marker='x', alpha=0.6, label='Неправильная единица')
-    plt.title(f'Точность={accuracy:.2f}')
+    plt.title(f'Точность = {accuracy:.2f}')
     plt.legend()
     plt.show()
-plot_results(x_train, y_train, x_test, y_test, y_predict1)
-
-
-
-
-
+plot_results(x_train, y_train, x_test, y_test, y_predict)
