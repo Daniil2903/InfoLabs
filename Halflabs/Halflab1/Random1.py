ee4abc48ee2f7e2d1fc3d88ba6d90ea99d50888e
Daniil2903 <m2406326@edu.misis.ru>
2025-04-27 16:18:03 +0300

    Add files via upload

---
+++ Halflabs/Halflab1/Random1.py
@@ -0,0 +1,5 @@
+import random
+random.seed(101)
+x1=random.randint(1,15)
+x2=random.randint(1,20)
+print(x1, x2)
---
+++ Halflabs/Halflab2/Random.py
@@ -0,0 +1,5 @@
+import random
+random.seed(1)
+x1=random.randint(1, 24)
+x2=random.randint(1, 28)
+print(x1, x2)
