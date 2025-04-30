e0135ec19f2c328036d806403352a3dea4afd64d
Daniil2903 <m2406326@edu.misis.ru>
2025-05-01 01:06:52 +0300

    Update Halflab5.py

--- Halflabs/Halflab5.py
+++ Halflabs/Halflab5.py
@@ -1,26 +1,30 @@
 import math
-func="math.exp(x)-x**2"
-difffunc="math.exp(x)-2*x"
-print(func, difffunc)
-x0=0
-epochs=100
-xlist=[]
-ylist=[]
-while epochs>=0:
-    for i in range(100):
-        xlist.append(x0-0.01*(math.exp(x0)-2*x0))
-        ylist.append(math.exp(x0)-x0**2)
-        x0=xlist[i]
-    epochs-=1
-import matplotlib.pyplot as plt
 import numpy as np
-plt.style.use('_mpl-gallery')
-x =xlist
-y =ylist
-sizes = np.random.uniform(15, 80, len(x))
-colors = np.random.uniform(15, 80, len(x))
-fig, ax = plt.subplots()
-ax.scatter(x, y, s=sizes, c=colors, vmin=0, vmax=100)
-ax.set(xlim=(-1), xticks=np.arange(2),
-       ylim=(0), yticks=np.arange(2))
+import matplotlib.pyplot as plt
+def f(x):
+    return np.exp(x)-x**2
+def df(x):
+    return np.exp(x)-2*x
+x0=0
+speed=0.01
+epochs=100
+x_path=[x0]
+y_path=[f(x0)]
+for _ in range(epochs):
+    x0=x0-speed*df(x0)
+    x_path.append(x0)
+    y_path.append(f(x0))
+x_vals=np.linspace(-2, 2, 400)
+plt.figure(figsize=(10, 6))
+plt.plot(x_vals, f(x_vals), label=r'$y = e^x - x^2$', color='blue', zorder=5)
+plt.scatter(x_path, y_path, c='orange', label='Градиентный спуск')
+for i in range(len(x_path)-1):
+    plt.arrow(x_path[i], y_path[i],
+              x_path[i+1]-x_path[i], y_path[i+1]-y_path[i],
+              head_width=0.02, length_includes_head=True, color='orange')
+plt.title("Функция и процесс градиентного спуска")
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid(True)
+plt.legend()
 plt.show()
